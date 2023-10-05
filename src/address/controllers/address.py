7ada52c89621af944b02d3bdfb3b67910abbd2a8
Steven Goez <steven.goez@gmail.com>
2023-10-05 02:16:00 -0400

    Restructuration2.0

--- src/address/controllers/address.py
+++ src/address/controllers/address.py
@@ -1,74 +1,64 @@
+from typing import List
 
-from fastapi import HTTPException, APIRouter
-from src.address.models.address import Address
-from src.address.services.address import  create_address
+from fastapi import APIRouter, HTTPException
+
+from src.address.models.address import Address, AddressCreate
+from src.address.services.address import get_next_address_id
+from src.databaseSimulation.databases import addresses_db
 from src.user.services.user import get_user_by_email
 
 addressRouter=APIRouter()
 
 
-# Ruta para crear una nueva dirección
+# Ruta para obtener todas las direcciones
+@addressRouter.get("/addresses/", response_model=List[Address])
+async def get_addresses():
+    return addresses_db
+
 @addressRouter.post("/addresses/", response_model=Address)
-async def create_address_route(email: str, address_create: Address):
+async def create_address(email: str, address_create: Address):
+    validate_address_create(address_create)  # Llama a la función de validación
+
     user = get_user_by_email(email)
     if user is None:
         raise HTTPException(status_code=404, detail="User with provided email does not exist")
 
-    address = create_address(user, address_create)
+    next_address_id = get_next_address_id()
+    address = create_address_instance(address_create, next_address_id)
 
-#     return address
-# # Ruta para crear una nueva dirección
-# @addressRouter.post("/addresses/", response_model=Address)
-# async def create_address(email: str, address_create: Address):
-#     # Validar que los campos obligatorios no estén vacíos ni tengan valores genéricos
-#     if (
-#         not address_create.address_1.strip() or
-#         not address_create.city.strip() or
-#         not address_create.state.strip() or
-#         not address_create.zip.strip() or
-#         not address_create.country.strip() or
-#         address_create.address_1.lower() == "string" or
-#         address_create.city.lower() == "string" or
-#         address_create.state.lower() == "string" or
-#         address_create.zip.lower() == "string" or
-#         address_create.country.lower() == "string"
-#     ):
-#         raise HTTPException(status_code=400, detail="Please provide valid data")
-#
-#     # Validar el formato del correo electrónico
-#     if not valid_email(email):
-#         raise HTTPException(status_code=400, detail="Provide a valid email (e.g., example@domain.com or example@domain.org)")
-#
-    # # Verificar si el correo electrónico proporcionado corresponde a un usuario existente
-    # user = get_user_by_email(email)
-    # if user is None:
-    #     raise HTTPException(status_code=404, detail="User with provided email does not exist")
-#
-#     # Obtener el próximo address_id
-#     next_address_id = get_next_address_id()
-#
-#     # Crear una instancia de Address con address_id autogenerado
-#     address = Address(
-#         address_id=next_address_id,
-#         address_1=address_create.address_1,
-#         address_2=address_create.address_2,
-#         city=address_create.city,
-#         state=address_create.state,
-#         zip=address_create.zip,
-#         country=address_create.country
-#     )
-#
-#     # Agregar la dirección a la lista
-#     addresses_db.append(address)
-#
-#     # Asociar la dirección al usuario
-#     user.addresses.append(address)
-#
-#     return address
-#
-#
-# # Ruta para obtener todas las direcciones
-# @addressRouter.get("/addresses/", response_model=List[Address])
-# async def get_addresses():
-#     return addresses_db
+    addresses_db.append(address)
+    user.addresses.append(address)
+
+    return address
+
+
+def validate_address_create(address_create: Address):
+    if (
+        not address_create.address_1.strip() or
+        not address_create.city.strip() or
+        not address_create.state.strip() or
+        not address_create.zip.strip() or
+        not address_create.country.strip() or
+        address_create.address_1.lower() == "string" or
+        address_create.city.lower() == "string" or
+        address_create.state.lower() == "string" or
+        address_create.zip.lower() == "string" or
+        address_create.country.lower() == "string"
+    ):
+        raise HTTPException(status_code=400, detail="Please provide valid data")
+
+def create_address_instance(address_create: Address, address_id: int):
+    return Address(
+        address_id=address_id,
+        address_1=address_create.address_1,
+        address_2=address_create.address_2,
+        city=address_create.city,
+        state=address_create.state,
+        zip=address_create.zip,
+        country=address_create.country
+    )
+
+
+
+
 
--- src/user/controllers/user.py
+++ src/user/controllers/user.py
@@ -1,93 +1,71 @@
 from typing import List
-from fastapi import APIRouter
 
+from fastapi import APIRouter, HTTPException, Query
+
+from src.address.services.address import get_next_address_id
+from src.databaseSimulation.databases import users_db
 from src.user.models.user import User, UserCreate
-from src.user.services.user import get_user
+from src.user.services.user import get_next_user_id, valid_email
 
-userRouter = APIRouter()
-
-# Ruta para crear un nuevo usuario con direcciones
-@userRouter.post("/users/", response_model=User)
-async def create_user(user_create: UserCreate):
-    return create_user(user_create)
+userRouter=APIRouter()
 
 # Ruta para obtener todos los usuarios
 @userRouter.get("/users/", response_model=List[User])
 async def get_users():
-    return get_user
+    if not users_db:
+        raise HTTPException(status_code=404, detail="No users found")
+    return users_db
+
+@userRouter.post("/users/", response_model=User)
+async def create_user(user_create: UserCreate):
+    validate_user_create(user_create)  # Llama a la función de validación
+
+    next_user_id = get_next_user_id()
+    associate_address_ids(user_create.addresses)  # Asocia IDs de direcciones
+
+    user = User(
+        user_id=next_user_id,
+        first_name=user_create.first_name,
+        last_name=user_create.last_name,
+        email=user_create.email,
+        addresses=user_create.addresses
+    )
+
+    users_db.append(user)
+
+    return user
+
+
+def validate_user_create(user_create: UserCreate):
+    if any(u.email == user_create.email for u in users_db):
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    if (
+        not user_create.first_name.strip() or
+        not user_create.last_name.strip() or
+        not user_create.email.strip() or
+        not user_create.password.strip() or
+        user_create.first_name == "string" or
+        user_create.last_name == "string" or
+        user_create.email == "string"
+    ):
+        raise HTTPException(status_code=400, detail="Please provide valid data")
+
+    if not valid_email(user_create.email):
+        raise HTTPException(status_code=400, detail="Provide a valid email (e.g., example@domain.com or example@domain.org)")
+
+def associate_address_ids(addresses):
+    for address in addresses:
+        address.address_id = get_next_address_id()
+
 
 # Ruta para obtener usuarios filtrados por país
 @userRouter.get("/users-by-country/", response_model=List[User])
-async def get_users_by_country(country: str):
-    return get_users_by_country(country)
+async def get_users_by_country(country: str = Query(..., description="Filter users by country")):
+    # Filtrar usuarios por país
+    filtered_users = [user for user in users_db if any(address.country == country for address in user.addresses)]
 
-# userRouter=APIRouter()
-#
-#
-# # Ruta para crear un nuevo usuario con direcciones
-# @userRouter.post("/users/", response_model=User)
-# async def create_user(user_create: UserCreate):
-#     # Verificar si el correo electrónico ya está en uso
-#     if any(u.email == user_create.email for u in users_db):
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#
-#     # Validar que los campos obligatorios no estén vacíos ni tengan valores genéricos
-#     if (
-#         not user_create.first_name.strip() or
-#         not user_create.last_name.strip() or
-#         not user_create.email.strip() or
-#         not user_create.password.strip() or
-#         user_create.first_name == "string" or
-#         user_create.last_name == "string" or
-#         user_create.email == "string"
-#     ):
-#         raise HTTPException(status_code=400, detail="Please provide valid data")
-#
-#     # Validar el formato del correo electrónico
-#     if not valid_email(user_create.email):
-#         raise HTTPException(status_code=400, detail="Provide a valid email (e.g., example@domain.com or example@domain.org)")
-#
-#         # Validar la contraseña
-#     if not is_valid_password(user_create.password):
-#         raise HTTPException(status_code=400,
-#                             detail="Password must contain at least one uppercase letter, one lowercase letter, and one special character")
-#
-#     # Obtener el próximo user_id
-#     next_user_id = get_next_user_id()
-#
-#     # Asociar el user_id a las direcciones
-#     for address in user_create.addresses:
-#         address.address_id = get_next_address_id()
-#
-#     # Crear una instancia de User con user_id autogenerado y las direcciones
-#     user = User(
-#         user_id=next_user_id,
-#         first_name=user_create.first_name,
-#         last_name=user_create.last_name,
-#         email=user_create.email,
-#         addresses=user_create.addresses
-#     )
-#
-#     # Agregar el usuario a la lista
-#     users_db.append(user)
-#
-#     return user
-#
-# # Ruta para obtener todos los usuarios
-# @userRouter.get("/users/", response_model=List[User])
-# async def get_users():
-#     if not users_db:
-#         raise HTTPException(status_code=404, detail="No users found")
-#     return users_db
-#
-#
-# # Ruta para obtener usuarios filtrados por país
-# @userRouter.get("/users-by-country/", response_model=List[User])
-# async def get_users_by_country(country: str = Query(..., description="Filter users by country")):
-#     # Filtrar usuarios por país
-#     filtered_users = [user for user in users_db if any(address.country == country for address in user.addresses)]
-#
-#     if not filtered_users:
-#         raise HTTPException(status_code=404, detail=f"No users found for country: {country}")
-#
-#     return filtered_users
+    if not filtered_users:
+        raise HTTPException(status_code=404, detail=f"No users found for country: {country}")
+
+    return filtered_users
